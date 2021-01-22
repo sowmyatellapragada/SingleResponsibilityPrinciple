ad3f694f8f1ee13c1bab4c4e0eeffd17afdfda94
sowmyatellapragada <63146223+sowmyatellapragada@users.noreply.github.com>
2021-01-22 19:59:51 +0530

    Create Program.cs

---
+++ Rating/Program.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace singleresponsibilityprinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ardalis Insurance Rating System Starting...");
+
+            var engine = new RatingEngine();
+            engine.Rate();
+
+            if (engine.Rating > 0)
+            {
+                Console.WriteLine($"Rating: {engine.Rating}");
+            }
+            else
+            {
+                Console.WriteLine("No rating produced.");
+            }
+
+        }
+    }
+}
